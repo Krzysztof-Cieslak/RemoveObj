d579ecb7451b340764f9302873b627043be921c8
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2018-05-31 16:34:53 +0100

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main _ =
+    try
+        let dir = Directory.GetCurrentDirectory()
+
+        Directory.GetFiles(dir, "*.*proj", SearchOption.AllDirectories)
+        |> Seq.map (fun p ->
+            let p = Path.GetDirectoryName p
+            Path.Combine (p, "obj")
+        )
+        |> Seq.iter (Directory.Delete)
+
+        0
+    with
+    | ex ->
+        printfn "%s" ex.Message
+        -1

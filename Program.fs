7d3a01f2956d332226c3fbaccdddc6f58be59fc1
Enrico Sada <enrico@sada.io>
2018-08-21 15:09:22 +0200

    add Argu for cli args

--- Program.fs
+++ Program.fs
@@ -1,7 +1,16 @@
 ﻿open System.IO
+open Argu
 
-[<EntryPoint>]
-let main _ =
+type CLIArguments =
+    | Bin
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Bin _ -> "remove also bin directories."
+
+let removeobjs () =
+
     try
         let dir = Directory.GetCurrentDirectory()
 
@@ -19,3 +28,23 @@ let main _ =
     | ex ->
         printfn "%s" ex.Message
         -1
+
+[<EntryPoint>]
+let main argv =
+    try
+        let parser = ArgumentParser.Create<CLIArguments>(programName = "removeobj")
+
+        let results = parser.Parse argv
+
+        let andBin = results.Contains Bin
+
+        removeobjs ()
+    with
+    | :? ArguParseException as ex ->
+        printfn "%s" ex.Message
+        match ex.ErrorCode with
+        | ErrorCode.HelpText -> 0
+        | _ -> -2
+    | ex ->
+        printfn "%s" ex.Message
+        -3
